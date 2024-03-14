f8f658bed4f952d8f71ce3a396e88152a201c984
mateo.dischler <mateo.dischler@student-cs.fr>
2024-03-14 11:58:37 +0100

    created ModeSelection to update the mode

---
+++ src/components/ModeSelection.js
@@ -0,0 +1,35 @@
+import WordsToWrite from "./WordsToWrite";
+import UpdateText from "./UpdateText";
+
+function ModeSelection({isModeChosen, setisModeChosen, isEndLess, setIsEndLess }) {
+    return(
+        <div>
+            <input 
+                type = "button"
+                value = "Normal Mode"
+                acceskey = "n"
+                onClick = {() => {
+                    setisModeChosen(true);
+                    //console.log(isEndLess)
+                    //console.log(isModeChosen)
+                }}
+                
+            />
+
+            <input 
+                type = "button"
+                value = "Endless Mode"
+                acceskey = "n"
+                onClick = {() => {
+                    setisModeChosen(true);
+                    setIsEndLess(true);
+                    //console.log(isEndLess);
+                    //console.log(isModeChosen);
+                }}
+            />
+        </div>
+    )
+
+}
+
+export default ModeSelection

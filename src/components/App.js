0ccb53cf4b9cb5ff4c0b161f591ce3b00d6c2118
mateo.dischler <mateo.dischler@student-cs.fr>
2024-03-14 11:59:11 +0100

    added a checker for the endlessMode, updates in ModeSelection.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,15 +9,16 @@ function App() {
   //console.log("App is rendered")
   const [textToShow, setTextToShow] = useState("");
   const [isModeChosen, setIsModeChosen] = useState(false);
+  const [isEndLess, setIsEndLess] = useState(false)
   
   const modeSelection = (
-    <ModeSelection isModeChosen={isModeChosen} setisModeChosen={setIsModeChosen}/>
+    <ModeSelection isModeChosen={isModeChosen} setisModeChosen={setIsModeChosen} isEndLess={isEndLess} setIsEndLess={setIsEndLess} />
   );
 
   const dactylo = (
     <div>
       <WordsToWrite textToShow={textToShow} setTextToShow={setTextToShow} />
-      <UpdateText textToShow={textToShow}/>
+      <UpdateText textToShow={textToShow} isEndLess={isEndLess}/>
     </div>
   );
 

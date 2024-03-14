5af70dae1bef812db8673e2361d7d4e9e080c4ae
mateo.dischler <mateo.dischler@student-cs.fr>
2024-03-14 11:29:54 +0100

    conditional return in App,js, returns the mode selection page by default and the dactylo once the mode has been selected

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,31 @@ import { useState } from 'react';
 import '../styles/App.css';
 import WordsToWrite from './WordsToWrite';
 import UpdateText from './UpdateText'
+import ModeSelection from './ModeSelection';
 
 function App() {
 
   //console.log("App is rendered")
-  const [textToShow, setTextToShow] = useState("")
-    
-  return (
+  const [textToShow, setTextToShow] = useState("");
+  const [isModeChosen, setIsModeChosen] = useState(false);
+  
+  const modeSelection = (
+    <ModeSelection isModeChosen={isModeChosen} setisModeChosen={setIsModeChosen}/>
+  );
+
+  const dactylo = (
     <div>
       <WordsToWrite textToShow={textToShow} setTextToShow={setTextToShow} />
       <UpdateText textToShow={textToShow}/>
     </div>
   );
+
+ return (
+  <div>
+    {isModeChosen ? dactylo : modeSelection}
+  </div>
+ );
+  
 }
 
 export default App;

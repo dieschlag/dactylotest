b5ccfd9956873e24e92beaeaa73c2380359b5442
mateo.dischler <mateo.dischler@student-cs.fr>
2024-03-14 10:46:37 +0100

    used a useEffect to fetch the wordsToWrite so that GenerateText is called only once

--- src/components/WordsToWrite.js
+++ src/components/WordsToWrite.js
@@ -1,14 +1,19 @@
 import GenerateText from "./GenerateText"
+import {useEffect} from 'react'
 
 function WordsToWrite({textToShow, setTextToShow}) {
 
-    const fetchText = async () => {
-        let generatedWords = await GenerateText();
-        //console.log(generatedWords);
-        setTextToShow(generatedWords);
-    }
+    console.log("bonjour")
 
-    fetchText()
+    useEffect(() => {
+        const fetchText = async () => {
+            let generatedWords = await GenerateText();
+            //console.log(generatedWords);
+            setTextToShow(generatedWords);
+        }
+        fetchText();
+    }, []
+    )
     return(
         <div>
             <h2>Mots à écrire</h2>

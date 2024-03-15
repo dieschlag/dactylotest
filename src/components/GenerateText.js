e30a1aea97fa31dd85eef651efb07fa253785fa9
mateo.dischler <mateo.dischler@student-cs.fr>
2024-03-15 19:27:46 +0100

    removed comment

--- src/components/GenerateText.js
+++ src/components/GenerateText.js
@@ -2,7 +2,7 @@ function GenerateText() {
 
     let randomNumber = Math.floor(Math.random()*10) + 1;
     let filePath = `../assets/text${randomNumber}.txt`;
-    console.log(filePath);
+    
 
     const context = require.context('../assets', true);
     const raw = context(`./text${randomNumber}.txt`);

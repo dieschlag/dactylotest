1c2e6ea0267d8076ae1e22798b74746b2041c960
mateo.dischler <mateo.dischler@student-cs.fr>
2024-06-27 11:48:32 +0200

    removed logs

--- src/components/ModeDisplay.js
+++ src/components/ModeDisplay.js
@@ -1,6 +1,5 @@
-function ModeDisplay(mode) {
-    console.log(mode);
-    return <h2><em>{mode=="normal" ? "Mode Normal" : "Mode Endless"}</em></h2>
+function ModeDisplay({mode}) {
+    return <h2><em>{mode==="normal" ? "Mode Normal" : "Mode Endless"}</em></h2>
 }
 
 export default ModeDisplay;
